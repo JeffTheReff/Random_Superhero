489731d16e3d3b93b0ecf406c692bb97319e915a
JeffTheReff <dahearn23@punahou.edu>
2016-06-30 13:57:23 -1000

    SOOP IS SOOP
    okay? mkay?

--- js/index.js
+++ js/index.js
@@ -1,7 +1,30 @@
-var Heros = ["Batman", "Superman", "Green Lantern", "Deadpool", "Hulk", "Captain America", "Thor", 
-"Spiderman", "Wonderwoman", "Ant Man", "Iron Man", "Black Panther", "Doctor Strange", "Wolverine",
-"Black Widow", "Ultron", "Loki", "Red Skull", "Magneto", "Scarecrow", "Poison Ivy", "Green Goblin",
-"Hawkeye", "Quicksilver", "War Machine"]
+var Heros = ["<img src='../assets/Batman.jpg'>",
+"<img src='../assets/Superman.jpg'>",
+"<img src='../assets/GreenLantern.jpg'>",
+"<img src='../assets/Deadpool.jpg'>",
+"<img src='../assets/Hulk.jpg'>",
+"<img src='../assets/Captain.jpg'>",
+"<img src='../assets/Thor.jpg'>",
+"<img src='../assets/Spider.jpg'>",
+"<img src='../assets/Wonder.jpg'>",
+"<img src='../assets/Ant.jpg'>",
+"<img src='../assets/Iron.jpg'>",
+"<img src='../assets/Black.jpg'>",
+"<img src='../assets/Doctor.jpg'>",
+"<img src='../assets/Wolverine.jpg'>",
+"<img src='../assets/BlackWidow.jpg'>",
+"<img src='../assets/Ultron.jpg'>",
+"<img src='../assets/Loki.jpg'>",
+"<img src='../assets/Red.jpg'>",
+"<img src='../assets/Magneto.jpg'>",
+"<img src='../assets/Scare.jpg'>",
+"<img src='../assets/Poison.jpg'>",
+"<img src='../assets/Green.jpg'>",
+"<img src='../assets/Hawk.jpg'>",
+"<img src='../assets/Quick.jpg'>",
+"<img src='../assets/War.jpg'>",
+"<img src='../assets/Flash.jpg'>"
+]
 
 function pickHero(){
 
@@ -9,7 +32,7 @@ var hero1 = Math.floor(Math.random()*Heros.length);
 
 var hero2 = Math.floor(Math.random()*Heros.length);
 
-document.getElementById('super').innerHTML = "<h1>"+Heros[hero1] + " vs "+Heros[hero2]+"</h1>"
+document.getElementById('super').innerHTML = "<h1>"+Heros[hero1] + " vs"+Heros[hero2]+"</h1>"
 
 
 }
